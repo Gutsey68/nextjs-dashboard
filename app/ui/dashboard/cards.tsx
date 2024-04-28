2faf20ebd03be04cb136ef62b51503bc032bf720
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-04-28 16:00:44 +0200

    Static and dynamic randering

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,3 +1,4 @@
+import { fetchCardData } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 import {
   BanknotesIcon,
@@ -14,6 +15,12 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: yooo */}
